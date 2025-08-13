d11e166668b6e55a9317b6d19edf755dda011d09
ProfVito25 <vitomaggio64@gmail.com>
2025-08-13 14:51:04 +0200

    Initial commit

--- scripts/definitions.js
+++ scripts/definitions.js
@@ -43,7 +43,7 @@ const QMAKE_SEND_WEBAPP = 4;
 const QMAKE_SEND_NODEJS = 5;
 const QMAKE_SEND_NODEJS_LOCAL = 6;  
 
-const QUIZ_KEYWORD = "Q433wo6rdvzBOi3v";
+const QUIZ_KEYWORD = "Sm7zNOwaincM3gh3";
 
 const USER_LOGIN_ANONYMOUS = "Anonymous";
 const USER_EMAIL_ANONYMOUS = "anonymous@anonymous.nowhere";
